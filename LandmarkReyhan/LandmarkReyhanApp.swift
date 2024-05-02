dcff6c0ec6ad24e9c54914e46aec4e14159ec78b
rjasonnn <reyhanjason@gmail.com>
2024-05-02 13:17:24 +0700

    Initial Commit

---
+++ LandmarkReyhan/LandmarkReyhanApp.swift
@@ -0,0 +1,29 @@
+
+import SwiftUI
+
+@main
+struct LandmarkReyhanApp: App {
+    @State private var modelData = ModelData()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(modelData)
+        }
+        #if !os(watchOS)
+        .commands {
+            LandmarkCommands()
+        }
+        #endif
+
+        #if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
+
+        #if os(macOS)
+        Settings {
+            LandmarkSettings()
+        }
+        #endif
+    }
+}
---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,14 @@
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+#Preview {
+    ContentView()
+        .environment(ModelData())
+}
